c7d8bbf87292ffc63b51e95d26ac415f1148ef38
joao01sb <bonfimnew@gmail.com>
2025-09-13 13:16:40 -0300

    feat: Add kotlinx-coroutines-test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.room.ktx)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
