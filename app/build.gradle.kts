a2dc0a1706f5b6cad7cedbdf7f8b86e1ae5b0581
joao01sb <bonfimnew@gmail.com>
2025-09-18 21:45:45 -0300

    feat: Add Material 3 Window Size Class library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material3.window.size.class1)
 
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
