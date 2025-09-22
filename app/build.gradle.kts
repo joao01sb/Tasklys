b3b1ff99385acb07a227ab78507e573ce0181193
joao01sb <bonfimnew@gmail.com>
2025-09-22 20:58:21 -0300

    feat: Add ViewModel Compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material3.window.size.class1)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
