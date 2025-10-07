e54f6287029b3f2bd23f11e1243c85b4fbf27fb1
joao01sb <bonfimnew@gmail.com>
2025-10-06 22:16:00 -0300

    feat: Implement AppContainer for NoteViewModel and navigation handling

--- app/src/main/java/com/joao01sb/tasklys/MainActivity.kt
+++ app/src/main/java/com/joao01sb/tasklys/MainActivity.kt
@@ -4,7 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.joao01sb.tasklys.features.notes.presentation.NoteListScreen
+import com.joao01sb.tasklys.features.notes.presentation.NoteScreen
+import com.joao01sb.tasklys.features.notes.presentation.NoteViewModel
+import com.joao01sb.tasklys.features.notes.presentation.NoteViewModelFactory
 import com.joao01sb.tasklys.ui.theme.TasklysTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.joao01sb.tasklys.features.notes.presentation.NoteDetailsScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +23,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TasklysTheme {
-
+                AppContainer()
             }
         }
     }
+}
+
+@Composable
+fun AppContainer() {
+
+    val context = LocalContext.current
+    val appContainer = (context.applicationContext as App).container
+
+    val viewmodel: NoteViewModel = viewModel(
+        factory = NoteViewModelFactory(
+            addNote = appContainer.addNote,
+            allNotes = appContainer.allNotes,
+            deleteAllNotes = appContainer.deleteAllNotes,
+            deleteNote = appContainer.deleteNote,
+            getNoteById = appContainer.getNoteById,
+            updateNote = appContainer.updateNote,
+            getNotesByFilter = appContainer.getNotesByFilter
+        )
+    )
+
+    val uiState by viewmodel.uiState.collectAsStateWithLifecycle()
+
+    when (viewmodel.currentScreen) {
+        NoteScreen.LIST -> NoteListScreen(
+            state = uiState,
+            onNoteClick = {},
+            onAddNoteClick = {},
+            searchQuery = viewmodel.currentQuery,
+            selectedFilter = viewmodel.selectedFilter,
+            onSearchQueryChange = {
+                viewmodel.onQueryChange(it)
+            },
+            onFilterChange = {
+                viewmodel.updateSelectedFilter(it)
+            },
+            onNoteComplete = {
+
+            },
+            onNoteDelete = {
+
+            },
+            onDeleteAllClick = {
+
+            }
+        )
+        NoteScreen.DETAIL -> {
+            NoteDetailsScreen() 
+        }
+    }
+
 }
